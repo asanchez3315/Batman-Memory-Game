f9b0059f7bf10a6694e7636c7e6287e3c2dd7bc0
Alexander Sanchez <asanchez3315@gmail.com>
2019-05-13 18:21:30 -0400

    Updates

---
+++ precache-manifest.98d3d1a5b271a62a359cae7b4cc3fbe3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aafa9935e2ca2e67c3faa20a1ca2698",
+    "url": "/Batman-Memory-Game/index.html"
+  },
+  {
+    "revision": "1089fc897a1903f21fc8",
+    "url": "/Batman-Memory-Game/static/css/main.bd6ff72b.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Batman-Memory-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "1089fc897a1903f21fc8",
+    "url": "/Batman-Memory-Game/static/js/main.5d99650a.chunk.js"
+  },
+  {
+    "revision": "487a0aac4722fe6f78ba",
+    "url": "/Batman-Memory-Game/static/js/runtime~main.63750ea4.js"
+  },
+  {
+    "revision": "b5ecdc656ebb6aacc423f66690ed08d6",
+    "url": "/Batman-Memory-Game/static/media/1.b5ecdc65.jpg"
+  },
+  {
+    "revision": "b2839b799000e066dfbb6ae755ce19fe",
+    "url": "/Batman-Memory-Game/static/media/10.b2839b79.jpg"
+  },
+  {
+    "revision": "445f653dbde823375afd1e0418d111b6",
+    "url": "/Batman-Memory-Game/static/media/11.445f653d.jpg"
+  },
+  {
+    "revision": "60c503ffe050404904139168426f5133",
+    "url": "/Batman-Memory-Game/static/media/12.60c503ff.jpg"
+  },
+  {
+    "revision": "fbabf1975b844804fb073329eaa4a95e",
+    "url": "/Batman-Memory-Game/static/media/2.fbabf197.jpg"
+  },
+  {
+    "revision": "ac7b13431c8acd1031333351669f09d0",
+    "url": "/Batman-Memory-Game/static/media/3.ac7b1343.jpg"
+  },
+  {
+    "revision": "4e8c35fb0f872083252942384e10677c",
+    "url": "/Batman-Memory-Game/static/media/4.4e8c35fb.jpg"
+  },
+  {
+    "revision": "9796b4572505d6bad357be656ebf2198",
+    "url": "/Batman-Memory-Game/static/media/5.9796b457.jpg"
+  },
+  {
+    "revision": "92f50dd54fe928caaccc6f8da960709e",
+    "url": "/Batman-Memory-Game/static/media/6.92f50dd5.jpg"
+  },
+  {
+    "revision": "29692bed56b62c5cb9a79d0b4d319934",
+    "url": "/Batman-Memory-Game/static/media/7.29692bed.jpg"
+  },
+  {
+    "revision": "b95d569639d56cc136fde3155a5dd013",
+    "url": "/Batman-Memory-Game/static/media/8.b95d5696.jpg"
+  },
+  {
+    "revision": "a34313470db1d15ac101819f1f61826d",
+    "url": "/Batman-Memory-Game/static/media/9.a3431347.jpg"
+  },
+  {
+    "revision": "0340b4590a0ed3c31f4f3d9628feb652",
+    "url": "/Batman-Memory-Game/static/media/desktop.0340b459.ini"
+  }
+]);
